0e2433871cf5f396ce401a3c7b42dbae2e370ea2
Joelson Botelho <107374263+JoelsonBotelho@users.noreply.github.com>
2026-07-22 01:19:14 +0100

    chore: initial Spring project setup

---
+++ src/main/java/dev/java10x/GeladeiraMagicaIA/GeladeiraMagicaIaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.GeladeiraMagicaIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeladeiraMagicaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeladeiraMagicaIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/GeladeiraMagicaIA/GeladeiraMagicaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.GeladeiraMagicaIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeladeiraMagicaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
